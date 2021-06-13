7f32feebba90ab7709ca8a1449bd8f8b82f0c858
Murdoc84 <murdoc84@gmail.com>
2021-06-13 19:56:05 +0200

    init

---
+++ Main.Designer.cs
@@ -0,0 +1,240 @@
+﻿
+namespace CThru
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonImageClipboard = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonRed1P = new System.Windows.Forms.Button();
+            this.buttonInc1P = new System.Windows.Forms.Button();
+            this.buttonDec10P = new System.Windows.Forms.Button();
+            this.buttonDec5P = new System.Windows.Forms.Button();
+            this.buttonInc5P = new System.Windows.Forms.Button();
+            this.buttonInc10P = new System.Windows.Forms.Button();
+            this.labelZoom = new System.Windows.Forms.Label();
+            this.labelOpacity = new System.Windows.Forms.Label();
+            this.radioButton100P = new System.Windows.Forms.RadioButton();
+            this.radioButton75P = new System.Windows.Forms.RadioButton();
+            this.radioButton50P = new System.Windows.Forms.RadioButton();
+            this.radioButton25P = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonImageClipboard
+            // 
+            this.buttonImageClipboard.Location = new System.Drawing.Point(2, 3);
+            this.buttonImageClipboard.Name = "buttonImageClipboard";
+            this.buttonImageClipboard.Size = new System.Drawing.Size(183, 23);
+            this.buttonImageClipboard.TabIndex = 0;
+            this.buttonImageClipboard.Text = "Get image from clipboard";
+            this.buttonImageClipboard.UseVisualStyleBackColor = true;
+            this.buttonImageClipboard.Click += new System.EventHandler(this.buttonImageClipboard_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(2, 32);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // buttonRed1P
+            // 
+            this.buttonRed1P.Location = new System.Drawing.Point(548, 4);
+            this.buttonRed1P.Name = "buttonRed1P";
+            this.buttonRed1P.Size = new System.Drawing.Size(41, 23);
+            this.buttonRed1P.TabIndex = 2;
+            this.buttonRed1P.Text = "-1%";
+            this.buttonRed1P.UseVisualStyleBackColor = true;
+            this.buttonRed1P.Click += new System.EventHandler(this.buttonRed1P_Click);
+            // 
+            // buttonInc1P
+            // 
+            this.buttonInc1P.Location = new System.Drawing.Point(592, 4);
+            this.buttonInc1P.Name = "buttonInc1P";
+            this.buttonInc1P.Size = new System.Drawing.Size(41, 23);
+            this.buttonInc1P.TabIndex = 3;
+            this.buttonInc1P.Text = "+1%";
+            this.buttonInc1P.UseVisualStyleBackColor = true;
+            this.buttonInc1P.Click += new System.EventHandler(this.buttonInc1P_Click);
+            // 
+            // buttonDec10P
+            // 
+            this.buttonDec10P.Location = new System.Drawing.Point(460, 4);
+            this.buttonDec10P.Name = "buttonDec10P";
+            this.buttonDec10P.Size = new System.Drawing.Size(41, 23);
+            this.buttonDec10P.TabIndex = 4;
+            this.buttonDec10P.Text = "-10%";
+            this.buttonDec10P.UseVisualStyleBackColor = true;
+            this.buttonDec10P.Click += new System.EventHandler(this.buttonDec10P_Click);
+            // 
+            // buttonDec5P
+            // 
+            this.buttonDec5P.Location = new System.Drawing.Point(504, 4);
+            this.buttonDec5P.Name = "buttonDec5P";
+            this.buttonDec5P.Size = new System.Drawing.Size(41, 23);
+            this.buttonDec5P.TabIndex = 5;
+            this.buttonDec5P.Text = "-5%";
+            this.buttonDec5P.UseVisualStyleBackColor = true;
+            this.buttonDec5P.Click += new System.EventHandler(this.buttonDec5P_Click);
+            // 
+            // buttonInc5P
+            // 
+            this.buttonInc5P.Location = new System.Drawing.Point(636, 4);
+            this.buttonInc5P.Name = "buttonInc5P";
+            this.buttonInc5P.Size = new System.Drawing.Size(41, 23);
+            this.buttonInc5P.TabIndex = 6;
+            this.buttonInc5P.Text = "+5%";
+            this.buttonInc5P.UseVisualStyleBackColor = true;
+            this.buttonInc5P.Click += new System.EventHandler(this.buttonInc5P_Click);
+            // 
+            // buttonInc10P
+            // 
+            this.buttonInc10P.Location = new System.Drawing.Point(680, 4);
+            this.buttonInc10P.Name = "buttonInc10P";
+            this.buttonInc10P.Size = new System.Drawing.Size(41, 23);
+            this.buttonInc10P.TabIndex = 7;
+            this.buttonInc10P.Text = "+10%";
+            this.buttonInc10P.UseVisualStyleBackColor = true;
+            this.buttonInc10P.Click += new System.EventHandler(this.buttonInc10P_Click);
+            // 
+            // labelZoom
+            // 
+            this.labelZoom.AutoSize = true;
+            this.labelZoom.Location = new System.Drawing.Point(422, 9);
+            this.labelZoom.Name = "labelZoom";
+            this.labelZoom.Size = new System.Drawing.Size(37, 13);
+            this.labelZoom.TabIndex = 8;
+            this.labelZoom.Text = "Zoom:";
+            // 
+            // labelOpacity
+            // 
+            this.labelOpacity.AutoSize = true;
+            this.labelOpacity.Location = new System.Drawing.Point(191, 8);
+            this.labelOpacity.Name = "labelOpacity";
+            this.labelOpacity.Size = new System.Drawing.Size(46, 13);
+            this.labelOpacity.TabIndex = 9;
+            this.labelOpacity.Text = "Opacity:";
+            // 
+            // radioButton100P
+            // 
+            this.radioButton100P.AutoSize = true;
+            this.radioButton100P.Location = new System.Drawing.Point(243, 7);
+            this.radioButton100P.Name = "radioButton100P";
+            this.radioButton100P.Size = new System.Drawing.Size(51, 17);
+            this.radioButton100P.TabIndex = 10;
+            this.radioButton100P.TabStop = true;
+            this.radioButton100P.Text = "100%";
+            this.radioButton100P.UseVisualStyleBackColor = true;
+            this.radioButton100P.CheckedChanged += new System.EventHandler(this.radioButton100P_CheckedChanged);
+            // 
+            // radioButton75P
+            // 
+            this.radioButton75P.AutoSize = true;
+            this.radioButton75P.Location = new System.Drawing.Point(291, 7);
+            this.radioButton75P.Name = "radioButton75P";
+            this.radioButton75P.Size = new System.Drawing.Size(45, 17);
+            this.radioButton75P.TabIndex = 11;
+            this.radioButton75P.TabStop = true;
+            this.radioButton75P.Text = "75%";
+            this.radioButton75P.UseVisualStyleBackColor = true;
+            this.radioButton75P.CheckedChanged += new System.EventHandler(this.radioButton75P_CheckedChanged);
+            // 
+            // radioButton50P
+            // 
+            this.radioButton50P.AutoSize = true;
+            this.radioButton50P.Location = new System.Drawing.Point(331, 7);
+            this.radioButton50P.Name = "radioButton50P";
+            this.radioButton50P.Size = new System.Drawing.Size(45, 17);
+            this.radioButton50P.TabIndex = 12;
+            this.radioButton50P.TabStop = true;
+            this.radioButton50P.Text = "50%";
+            this.radioButton50P.UseVisualStyleBackColor = true;
+            this.radioButton50P.CheckedChanged += new System.EventHandler(this.radioButton50P_CheckedChanged);
+            // 
+            // radioButton25P
+            // 
+            this.radioButton25P.AutoSize = true;
+            this.radioButton25P.Location = new System.Drawing.Point(371, 7);
+            this.radioButton25P.Name = "radioButton25P";
+            this.radioButton25P.Size = new System.Drawing.Size(45, 17);
+            this.radioButton25P.TabIndex = 13;
+            this.radioButton25P.TabStop = true;
+            this.radioButton25P.Text = "25%";
+            this.radioButton25P.UseVisualStyleBackColor = true;
+            this.radioButton25P.CheckedChanged += new System.EventHandler(this.radioButton25P_CheckedChanged);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 174);
+            this.Controls.Add(this.radioButton25P);
+            this.Controls.Add(this.radioButton50P);
+            this.Controls.Add(this.radioButton75P);
+            this.Controls.Add(this.radioButton100P);
+            this.Controls.Add(this.labelOpacity);
+            this.Controls.Add(this.labelZoom);
+            this.Controls.Add(this.buttonInc10P);
+            this.Controls.Add(this.buttonInc5P);
+            this.Controls.Add(this.buttonDec5P);
+            this.Controls.Add(this.buttonDec10P);
+            this.Controls.Add(this.buttonInc1P);
+            this.Controls.Add(this.buttonRed1P);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.buttonImageClipboard);
+            this.Name = "Main";
+            this.Text = "CThru";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonImageClipboard;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button buttonCloseTransparentWindow;
+        private System.Windows.Forms.Button buttonRed1P;
+        private System.Windows.Forms.Button buttonInc1P;
+        private System.Windows.Forms.Button buttonDec10P;
+        private System.Windows.Forms.Button buttonDec5P;
+        private System.Windows.Forms.Button buttonInc5P;
+        private System.Windows.Forms.Button buttonInc10P;
+        private System.Windows.Forms.Label labelZoom;
+        private System.Windows.Forms.Label labelOpacity;
+        private System.Windows.Forms.RadioButton radioButton100P;
+        private System.Windows.Forms.RadioButton radioButton75P;
+        private System.Windows.Forms.RadioButton radioButton50P;
+        private System.Windows.Forms.RadioButton radioButton25P;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,187 @@
+﻿using NHotkey;
+using NHotkey.WindowsForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CThru
+{
+    public partial class Main : Form
+    {
+        private Image clipboardImage;
+        private Image actualImage;
+        private int actualSizePercentage = 100;
+        private float opacity = 1.0f;
+        private bool nowClickableThrough = false;
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        const int GWL_EXSTYLE = -20;
+        const int WS_EX_LAYERED = 0x80000;
+        const int WS_EX_TRANSPARENT = 0x20;
+
+        public Main()
+        {
+            HotkeyManager.Current.AddOrReplace("ClickThrough", Keys.Alt | Keys.G, ClickThrough);
+            InitializeComponent();
+            this.AutoSize = true;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.MaximizeBox = false;
+            this.TopMost = true;
+            radioButton100P.Checked = true;
+        }
+
+        private void buttonImageClipboard_Click(object sender, EventArgs e)
+        {
+            clipboardImage = Clipboard.GetImage();
+            if (clipboardImage != null)
+            {
+                setImage(clipboardImage);
+            }
+        }
+
+        private void buttonDec10P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage -= 10;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+
+        private void buttonDec5P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage -= 5;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+
+        private void buttonRed1P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage--;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+
+        private void buttonInc1P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage++;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+
+        private void buttonInc5P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage += 5;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+
+        private void buttonInc10P_Click(object sender, EventArgs e)
+        {
+            actualSizePercentage += 10;
+            actualImage = ScaleByPercent(clipboardImage, actualSizePercentage);
+            setImage(actualImage);
+        }
+        private void setImage(Image image)
+        {
+            actualImage = image;
+            pictureBox.Size = new Size(actualImage.Width, actualImage.Height);
+            pictureBox.Image = actualImage;
+        }
+        public static Image ScaleByPercent(Image imgPhoto, int Percent)
+        {
+            float nPercent = ((float)Percent / 100);
+
+            int sourceWidth = (int)imgPhoto.Width;
+            int sourceHeight = (int)imgPhoto.Height;
+            int sourceX = 0;
+            int sourceY = 0;
+
+            int destX = 0;
+            int destY = 0;
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(destWidth, destHeight, PixelFormat.Format24bppRgb);
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            return bmPhoto;
+        }
+
+        private void ClickThrough(object sender, HotkeyEventArgs e)
+        {
+            Debug.WriteLine("HOTKEY!!!");
+            if (nowClickableThrough)
+            {
+                setNotClickableThrough();
+            } 
+            else
+            {
+                setClickableThrough();
+            }
+            nowClickableThrough = !nowClickableThrough;
+        }
+
+        private void setClickableThrough()
+        {
+            Debug.WriteLine("Now Clickable!!!");
+            var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            SetWindowLong(this.Handle, GWL_EXSTYLE, style | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+        }
+        private void setNotClickableThrough()
+        {
+            Debug.WriteLine("Now NOT Clickable!!!");
+            var style = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            SetWindowLong(this.Handle, GWL_EXSTYLE, style & ~WS_EX_TRANSPARENT);
+        }
+
+        private void radioButton100P_CheckedChanged(object sender, EventArgs e)
+        {
+            opacity = 1.0f;
+            setOpacity();
+        }
+
+        private void radioButton75P_CheckedChanged(object sender, EventArgs e)
+        {
+            opacity = 0.75f;
+            setOpacity();
+        }
+
+        private void radioButton50P_CheckedChanged(object sender, EventArgs e)
+        {
+            opacity = 0.5f;
+            setOpacity();
+        }
+
+        private void radioButton25P_CheckedChanged(object sender, EventArgs e)
+        {
+            opacity = 0.25f;
+            setOpacity();
+        }
+
+        private void setOpacity()
+        {
+            this.Opacity = opacity;
+        }
+    }
+}
